deea7d6aa10211159e90314bc3c28eae58fe2eea
rafalcl <rafalclab@gmail.com>
2018-01-12 14:11:06 +0100

    Configure Spring Web MVC

---
+++ src/main/java/pl/coderslab/apiback/app/AppConfig.java
@@ -0,0 +1,14 @@
+package pl.coderslab.apiback.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"pl.coderslab.apiback.bean",
+		"pl.coderslab.apiback.controller"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+}
---
+++ src/main/java/pl/coderslab/apiback/controller/TestController.java
@@ -0,0 +1,12 @@
+package pl.coderslab.apiback.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/test")
+	public String test() {
+		return "{test: ok}";
+	}
+}
